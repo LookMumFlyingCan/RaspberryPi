ca592570ceb58780a462939801956ef1332fc473
22h39 <17597456+22h39@users.noreply.github.com>
2021-07-03 16:39:27 +0200

    s

--- rpi_dummy/main.py
+++ rpi_dummy/main.py
@@ -1,23 +1,24 @@
 import serial
 import time
 
-s = serial.Serial('/dev/pts/5')
+s = serial.Serial('/dev/pts/4')
 
-lines = """8d4785bf990cad1870688b920bb0
-5d4785bff99ec3
-a0000f1ceb099b1e618c32eaa723
-8d4785bf990cad1870648bda51b0
-8d4785bf990cad18706c8c55ed94
-8d4785bf5879e1a90a196003be04
-a0000f1eff5e27207ff49463148a
-a0000f1eaee0000000000046c19f
-28000a3e0aff58"""
+lines = """8D40621D58C386435CC412692AD6
+8D40621D58C382D690C8AC2863A7
+8D40621B58C386435CC412692AD6
+8D40621B58C382D690C4AC2863A7
+8D40621D994457958838765B284F
+8D40621D9B06B6AF189400CBC33F
+2A00516D492B80
+8D40621D202CC371C32CE0576098
+420100f47d2042e3a5a2412fdd9941e7fbc041e3388e3fecc039230aa98f408fc2f53d0000000077becf3f0ad7a3400ad7a3400000b040b81ea5400000b040b81ea5400102c832"""
 
 for line in lines.split('\n'):
     for b in bytes(line + chr(10), 'utf-8'):
         print(b, ' ', end="")
     print()
     time.sleep(0.1)
-    s.write(bytes(line + chr(10), 'utf-8'))
+    buf = bytearray.fromhex(line)
+    s.write(buf + (b'\x00' * (128 - len(buf))))
 
 s.close()
